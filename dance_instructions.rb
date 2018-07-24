2ad88eb3b6d665ffa91cf15983052791aa323b88
hanam0822 <hanam0822@gmail.com>
2018-07-24 17:33:55 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -1,2 +1,5 @@
+def say_hello(your_name, name_two, name_three)
+  puts "Say hello #{your_name}! say hello #{name_two}! say hello #{name_three}"
+end
 
-# Code your methods below
+say_hello("hana", "madison", "alana")
